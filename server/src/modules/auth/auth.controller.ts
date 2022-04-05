0d0bc27fe80b0afd72721b714b55c57d0de44787
Ruslan Mirets <ruslan.mirets@gmail.com>
2022-04-05 11:11:42 +0300

    Guards [SERVER]

--- server/src/modules/auth/auth.controller.ts
+++ server/src/modules/auth/auth.controller.ts
@@ -1,5 +1,5 @@
+import { LocalAuthGuard } from './../../core/guards/local-auth.guard';
 import { Body, Controller, Post, UseGuards, Request } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 import { DoesUserExist } from 'src/core/guards/doesUserExist.guard';
 import { CreateUserDto } from '../user/dto/create-user.dto';
 import { AuthService } from './auth.service';
@@ -8,7 +8,7 @@ import { AuthService } from './auth.service';
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req) {
     return await this.authService.login(req.user);
