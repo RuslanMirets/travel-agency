72abc8c3188ff544a962a49689fab9502d236819
Ruslan Mirets <ruslan.mirets@gmail.com>
2022-04-06 15:04:01 +0300

    User menu [CLIENT]

--- client/components/Header/index.tsx
+++ client/components/Header/index.tsx
@@ -1,12 +1,45 @@
 import React from 'react';
 import styles from './Header.module.scss';
-import { Box, Container, Typography } from '@mui/material';
+import {
+  Avatar,
+  Box,
+  Container,
+  Divider,
+  IconButton,
+  ListItemIcon,
+  Menu,
+  MenuItem,
+  Typography,
+} from '@mui/material';
 import Link from 'next/link';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import { logout } from '../../store/actions/user';
 import { NavItem } from '../NavItem';
+import { AdminPanelSettings, Edit, Group, Logout, Person } from '@mui/icons-material';
+
+const adminItems = [
+  {
+    title: 'Пользователи',
+    href: '/users',
+    icon: <Group fontSize="small" />,
+  },
+  {
+    title: 'Редактировать',
+    href: '/edit',
+    icon: <Edit fontSize="small" />,
+  },
+];
 
 export const Header: React.FC = () => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   const dispatch = useAppDispatch();
   const { user } = useAppSelector((state) => state.user);
 
@@ -31,7 +64,58 @@ export const Header: React.FC = () => {
           </Box>
           <Box className={styles.actions} component="ul">
             {user ? (
-              <NavItem title="Выйти" href="#" onClick={handleLogout} />
+              <Box className={styles.user}>
+                <IconButton onClick={handleClick}>
+                  <Avatar alt={user.name} />
+                </IconButton>
+                <Menu
+                  sx={{ mt: '40px' }}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  anchorEl={anchorEl}
+                  id="account-menu"
+                  open={open}
+                  onClose={handleClose}
+                  onClick={handleClose}>
+                  <Link href="/profile">
+                    <MenuItem component="a">
+                      <ListItemIcon>
+                        {user.role[0].value === 'ADMIN' ? (
+                          <AdminPanelSettings fontSize="small" />
+                        ) : (
+                          <ListItemIcon>
+                            <Person fontSize="small" />
+                          </ListItemIcon>
+                        )}
+                      </ListItemIcon>
+                      Профиль
+                    </MenuItem>
+                  </Link>
+                  {user.role[0].value === 'ADMIN' &&
+                    adminItems.map((item, index) => (
+                      <Link key={index} href={item.href}>
+                        <MenuItem component="a">
+                          <ListItemIcon>{item.icon}</ListItemIcon>
+                          {item.title}
+                        </MenuItem>
+                      </Link>
+                    ))}
+                  <Divider variant="middle" />
+                  <MenuItem onClick={handleLogout}>
+                    <ListItemIcon>
+                      <Logout fontSize="small" />
+                    </ListItemIcon>
+                    Выйти
+                  </MenuItem>
+                </Menu>
+              </Box>
             ) : (
               <>
                 <NavItem title="Войти" href="/login" />
---
+++ client/pages/profile.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from 'next';
+import React from 'react';
+import MainLayout from '../layouts/MainLayout';
+
+const Profile: NextPage = () => {
+  return <MainLayout title="Профиль">Профиль</MainLayout>;
+};
+
+export default Profile;
